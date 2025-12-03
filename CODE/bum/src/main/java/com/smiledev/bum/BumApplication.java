c0c5aea334f46eb2fcf7c656463f13f62e8ede4b
Smile <longmoon2004@gmail.com>
2025-12-03 14:21:42 +0700

    Create new project

---
+++ CODE/bum/src/main/java/com/smiledev/bum/BumApplication.java
@@ -0,0 +1,13 @@
+package com.smiledev.bum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumApplication.class, args);
+	}
+
+}
---
+++ CODE/bum/src/test/java/com/smiledev/bum/BumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smiledev.bum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
