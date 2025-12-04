e6368eeb7c4272ff85c8b54d492c406afa4fc52a
Smile <longmoon2004@gmail.com>
2025-12-04 11:04:43 +0700

    Edit entity users and demo entity product. Add thymeleaf to properties

--- CODE/bum/src/main/java/com/smiledev/bum/entity/Products.java
+++ CODE/bum/src/main/java/com/smiledev/bum/entity/Products.java
@@ -1,4 +1,23 @@
 package com.smiledev.bum.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Products")
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    public Products() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
 }
--- CODE/bum/src/main/java/com/smiledev/bum/entity/Users.java
+++ CODE/bum/src/main/java/com/smiledev/bum/entity/Users.java
@@ -1,4 +1,145 @@
 package com.smiledev.bum.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Column(name = "full_name", length = 100)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", columnDefinition = "ENUM('admin', 'developer', 'user')")
+    private Role role;
+
+    @Column(name = "wallet_balance", precision = 15, scale = 2)
+    private BigDecimal walletBalance;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    // Các mối quan hệ
+    @ManyToOne
+    @JoinColumn(name = "ProId") //Cột fk trỏ sang table Products
+    private Products pro;
+
+    public Products getPro() {
+        return pro;
+    }
+
+    public void setPro(Products pro) {
+        this.pro = pro;
+    }
+
+
+    public Users() {
+    }
+
+    // Key tự tăng nên không có userID
+    public Users(String username, String email, String passwordHash, String fullName, Role role, BigDecimal walletBalance, boolean isActive, LocalDateTime createdAt) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.fullName = fullName;
+        this.role = role;
+        this.walletBalance = walletBalance;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+    }
+
+    public enum Role {
+        admin, developer, user
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public BigDecimal getWalletBalance() {
+        return walletBalance;
+    }
+
+    public void setWalletBalance(BigDecimal walletBalance) {
+        this.walletBalance = walletBalance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
