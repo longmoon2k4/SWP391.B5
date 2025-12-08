1633c18d4010131830481062c8e83223f05e9b9c
Smile <longmoon2004@gmail.com>
2025-12-08 10:12:27 +0700

    Update SRS and import missing liberty

--- CODE/bum/src/main/java/com/smiledev/bum/entity/Products.java
+++ CODE/bum/src/main/java/com/smiledev/bum/entity/Products.java
@@ -1,9 +1,20 @@
 package com.smiledev.bum.entity;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Products")
